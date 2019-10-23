3e915a010b9effeed00786464e1227c45434e94d
Nendae <ttp_laptop@TTP-Laptops-MacBook-Air.local>
2019-10-23 15:34:01 -0400

    done with binary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,24 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length < 2){
+		return array[0] === target
+	} else {
+		const left = array.slice(0, Math.floor(array.length/2))
+		const right = array.slice(Math.floor(array.length/2))
+
+		if(left[left.length-1]===target){
+			return true
+		} else if(left[left.length-1] > target) {
+			return binarySearch(left, target)
+		}
+		if (right[0]===target){
+			return true
+		} else if(right[0]< target) {
+			return binarySearch(right, target)
+	}
+	return false
+	}
 };
 
 /*
